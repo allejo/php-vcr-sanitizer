edec06e62c6858fcaa513ac275d13faaee7cbcd1
Vladimir Jimenez <allejo@me.com>
2020-07-28 17:30:52 -0700

    Ignore phpDoc return types for Config helpers

--- src/Config.php
+++ src/Config.php
@@ -16,41 +16,65 @@ abstract class Config
 {
     private static $options = array();
 
+    /**
+     * @return void
+     */
     public static function configureOptions(array $options)
     {
         self::$options = array_replace_recursive(self::defaultConfig(), $options);
     }
 
+    /**
+     * @return bool
+     */
     public static function ignoreReqHostname()
     {
         return self::$options['request']['ignoreHostname'];
     }
 
+    /**
+     * @return string[]
+     */
     public static function getReqIgnoredQueryFields()
     {
         return self::$options['request']['ignoreQueryFields'];
     }
 
+    /**
+     * @return string[]
+     */
     public static function getReqIgnoredHeaders()
     {
         return self::$options['request']['ignoreHeaders'];
     }
 
+    /**
+     * @return array<callable(string): string>
+     */
     public static function getReqBodyScrubbers()
     {
         return self::$options['request']['bodyScrubbers'];
     }
 
+    /**
+     * @return array<callable(string): string>
+     */
     public static function getReqPostFieldScrubbers()
     {
         return self::$options['request']['postFieldScrubbers'];
     }
 
+    /**
+     * @return string[]
+     */
     public static function getResIgnoredHeaders()
     {
         return self::$options['response']['ignoreHeaders'];
     }
 
+    /**
+     * @return array<callable(string): string>
+     */
     public static function getResBodyScrubbers()
     {
         return self::$options['response']['bodyScrubbers'];
